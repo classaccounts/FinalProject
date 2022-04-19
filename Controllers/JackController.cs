9abe6b205d11ebfffedcfb57721ae580b13747f6
Jack McHugh <mchughjk@mail.uc.edu>
2022-04-18 22:43:04 -0400

    Added about page information, updated my controller and pages

--- Controllers/JackController.cs
+++ Controllers/JackController.cs
@@ -1,16 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using FinalProjectWebServerApp.Models;
+using System.Linq;
 
 namespace FinalProjectWebServerApp.Controllers
 {
     public class JackController : Controller
     {
+        private AboutContext ctx;
+
+        public JackController(AboutContext ctx)
+        {
+            this.ctx = ctx;
+        }
         public IActionResult Hobby()
         {
-            return View();
+            return View(getViewModel());
         }
         public IActionResult Project()
         {
-            return View();
+            return View(getViewModel());
+        }
+
+        private JackHobbyProjectViewModel getViewModel() {
+            var jhpvm = new JackHobbyProjectViewModel();
+            jhpvm.Hobbies = ctx.Hobby.Where(s => s.StudentId == 1).ToList();
+            jhpvm.Projects = ctx.Project.Where(s => s.StudentId == 1).ToList();
+            return jhpvm;
         }
     }
 }
---
+++ Models/JackHobbyProjectViewModel.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using FinalProjectWebServerApp.Models;
+
+public class JackHobbyProjectViewModel
+{
+    public List<Project> Projects { get; set; }
+    public List<Hobby> Hobbies { get; set; } 
+}
