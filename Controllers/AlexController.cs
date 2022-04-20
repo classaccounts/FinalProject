03e608f8e209355a72392d4d2188716b31a19967
Alex Robinson <robin3a5@mail.uc.edu>
2022-04-20 13:34:43 -0400

    Project controller updated to use new model, hobby mispelling fixed, about page styles updated, images added

--- Controllers/AlexController.cs
+++ Controllers/AlexController.cs
@@ -1,16 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FinalProjectWebServerApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace FinalProjectWebServerApp.Controllers
 {
     public class AlexController : Controller
     {
+
+        private AboutContext ctx;
+
+        public AlexController(AboutContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
         public IActionResult Hobby()
         {
-            return View();
+            return View(getViewModel());
         }
         public IActionResult Project()
         {
-            return View();
+            return View(getViewModel());
+        }
+
+        private AlexHobbyProjectViewModel getViewModel() {
+            var ahpvm = new AlexHobbyProjectViewModel();
+            ahpvm.Hobbies = ctx.Hobby.Where(s => s.StudentId == 2).ToList();
+            ahpvm.Projects = ctx.Project.Where(s => s.StudentId == 2).ToList();
+            return ahpvm;
         }
     }
 }
---
+++ Models/AlexHobbyProjectViewModel.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using FinalProjectWebServerApp.Models;
+
+public class AlexHobbyProjectViewModel
+{
+    public List<Project> Projects { get; set; }
+    public List<Hobby> Hobbies { get; set; }
+
+}
